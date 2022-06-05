f89926e2abea5e4e3a645592d967623f01c19fca
htehrani98 <h.tehrani1377@gmail.com>
2022-06-05 22:29:15 +0430

    f

--- src/components/Library.js
+++ src/components/Library.js
@@ -1,13 +1,19 @@
 import React from "react";
 import LibrarySong from "./LibrarySong";
 
-const Library = ({ songs }) => {
+const Library = ({ songs, setCurrentSong }) => {
   return (
     <div className="library">
       <h2>Library</h2>
       <div className="library-songs">
         {songs.map((song, index) => (
-          <LibrarySong song={song} key={index} />
+          <LibrarySong
+            songs={songs}
+            song={song}
+            key={index}
+            setCurrentSong={setCurrentSong}
+            index={index}
+          />
         ))}
       </div>
     </div>
--- src/components/LibrarySong.js
+++ src/components/LibrarySong.js
@@ -1,8 +1,12 @@
 import React from "react";
 
-const LibrarySong = ({ song }) => {
+const LibrarySong = ({ song, songs, setCurrentSong, index }) => {
+  const songSelectHandler = () => {
+    const selectedSong = songs.filter((state) => state.index === index);
+    setCurrentSong(selectedSong[0]);
+  };
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={songSelectHandler}>
       <img alt={song.name} src={song.cover}></img>
       <div className="song-description">
         <h3>{song.name} </h3>
